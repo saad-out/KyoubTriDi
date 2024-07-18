cd1e8b93f15ce1d91eb2e5e6c55a2d9ef02c2d43
saad-out <outsaad03@gmail.com>
2024-07-18 07:04:37 +0100

    raycasting in 2D

--- srcs/render/raycasting.c
+++ srcs/render/raycasting.c
@@ -6,14 +6,14 @@
 /*   By: soutchak <soutchak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 04:14:03 by soutchak          #+#    #+#             */
-/*   Updated: 2024/07/18 06:10:04 by soutchak         ###   ########.fr       */
+/*   Updated: 2024/07/18 07:03:59 by soutchak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
 #define FOV (60 * (PI / 180)) // 60 degrees in radians
-#define NUM_RAYS (WIDTH / 100)
+#define NUM_RAYS (WIDTH / 10)
 
 double  normalizeAngle(double angle)
 {
@@ -23,6 +23,70 @@ double  normalizeAngle(double angle)
     if (new < 0)
         new += 2 * PI;
     return new;
+}
+
+// Function to return the point with the smallest coordinates
+// t_point min_point(t_point a, t_point b) {
+//     t_point min;
+
+//     if ((a.x < b.x) || (a.x == b.x && a.y < b.y)) {
+//         min = a;
+//     } else {
+//         min = b;
+//     }
+
+//     return min;
+// }
+
+double distance(t_point a, t_point b) {
+    return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+}
+
+t_point min_point(t_point a, t_point b, t_player *player) {
+    t_point p;
+    p.x = player->x;
+    p.y = player->y;
+    double dist_a = distance(p, a);
+    double dist_b = distance(p, b);
+    return dist_a < dist_b ? a : b;
+}
+
+t_point ver_intersection_distance(t_ray ray, t_player *player)
+{
+    t_point point;
+    double  xIntercept;
+    double  yIntercept;
+    double  xStep;
+    double  yStep;
+
+    // find vertical (y/x)(inter/step) values
+    xIntercept = floor(player->x / TILE_SIZE) * TILE_SIZE;
+    if (ray.facingRight)
+        xIntercept += TILE_SIZE;
+    yIntercept = player->y + ((xIntercept - player->x) * tan(ray.angle));
+    xStep = TILE_SIZE;
+    if (!ray.facingRight)
+        xStep *= -1;
+    yStep = xStep * tan(ray.angle);
+    if ((yStep > 0 && ray.facingUp) || (yStep < 0 && !ray.facingUp))
+        yStep *= -1;
+    
+    // find vertical intersection
+    point.x = xIntercept;
+    point.y = yIntercept;
+    if (!ray.facingRight)
+        point.x--;
+    while ((point.x > 0 && point.x < WIDTH) && (point.y > 0 && point.y < HEIGHT))
+    {
+        if (is_wall(point.x, point.y, get_data(NULL)))
+            break ;
+        else
+        {
+            point.x += xStep;
+            point.y += yStep;
+        }
+    }
+    return (point);
 }
 
 t_point hor_intersection_distance(t_ray ray, t_player *player)
@@ -89,7 +153,10 @@ void cast_rays(t_mlx *mlx, t_player *player) {
         printf(" ===========> Ray facingUp: %d facingRight: %d ==========>\n", ray[i].facingUp, ray[i].facingRight);
 
         t_point horInter = hor_intersection_distance(ray[i], player);
-        ft_draw_line(mlx, player->x, player->y, horInter.x, horInter.y, 0x00000000);
+        t_point verInter = ver_intersection_distance(ray[i], player);
+        t_point min = min_point(horInter, verInter, player);
+        printf("============ (%2.f,%2.f) (%2.f,%2.f) MIN is =>(%2.f,%2.f)\n", horInter.x, horInter.y, verInter.x, verInter.y, min.x, min.y);
+        ft_draw_line(mlx, player->x, player->y, min.x, min.y, 0x00000000);
         
         double ray_x = player->x + cos(rayangle) * 50;
         double ray_y = player->y + sin(rayangle) * 50;
